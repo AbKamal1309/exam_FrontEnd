defb7335613f378f972760b1a5f7bfddaa546792
AbKamal1309 <Kamal@gmail.com>
2026-09-25 21:55:40 +0000

    Ajout badge Google Play flottant sur la landing page (fermable, mémorisé, trilingue)

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
@@ -11,6 +11,28 @@ import { LangService } from '../../services/lang.service';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  readonly playStoreUrl = 'https://play.google.com/store/apps/details?id=com.acoidemy.amtihan';
+  playBadgeDismissed = false;
+
+  private readonly PLAY_BADGE_KEY = 'amtihan_play_badge_dismissed';
+
   constructor(public lang: LangService) {}
+
+  ngOnInit() {
+    this.playBadgeDismissed = localStorage.getItem(this.PLAY_BADGE_KEY) === 'true';
+  }
+
+  get playBadgeImg(): string {
+    const supported = ['en', 'fr', 'ar'];
+    const code = supported.includes(this.lang.lang()) ? this.lang.lang() : 'en';
+    return `https://play.google.com/intl/${code}_us/badges/static/images/badges/${code}_badge_web_generic.png`;
+  }
+
+  dismissPlayBadge(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.playBadgeDismissed = true;
+    localStorage.setItem(this.PLAY_BADGE_KEY, 'true');
+  }
 }
